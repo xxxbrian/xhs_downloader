c82f8206b326cec5f469ebe7217e6e94162b6f79
Bojin Li <bojinxx@gmail.com>
2024-01-25 15:26:31 +0800

    fix: Some notes do not have og:image, using a new method to retrieve the image URL

--- src/lib.rs
+++ src/lib.rs
@@ -7,25 +7,27 @@ use regex::Regex;
 pub async fn fetch_image_tokens(url: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let resp = reqwest::get(url).await?.text().await?;
     let document = Html::parse_document(&resp);
-    let selector = Selector::parse("meta").unwrap();
+    let selector = Selector::parse("script").unwrap();
     let mut image_tokens = HashSet::new();
 
     // http://sns-webpic-qc.xhscdn.com/202401241638/e397dfb12da8a8d418432ddcfa3d551c/{token}!nd_dft_wlteh_jpg_3
     let re = Regex::new(r"/([^\/]+?)!").unwrap();
 
-    // iterate over all meta elements
-    for element in document.select(&selector) {
-        if element.value().attr("name") == Some("og:image") {
-            if let Some(content) = element.value().attr("content") {
-                if let Some(captures) = re.captures(content) {
-                    if let Some(token) = captures.get(1) {
-                        image_tokens.insert(token.as_str().to_string());
-                    }
-                }
+    for script in document.select(&selector) {
+        let script_content = script.inner_html();
+        if script_content.starts_with("window.__INITIAL_STATE__=") {
+            let json_content = script_content.trim_start_matches("window.__INITIAL_STATE__=").replace("undefined", "0").replace("null", "0");
+            let json: serde_json::Value = serde_json::from_str(&json_content)?;
+            let first_note_id = json["note"]["firstNoteId"].as_str().unwrap();
+            let image_list = json["note"]["noteDetailMap"].get(first_note_id).unwrap()["note"]["imageList"].as_array().unwrap();
+            for image in image_list {
+                let image_url = image["urlDefault"].as_str().unwrap();
+                let token = re.captures(image_url).unwrap().get(1).unwrap().as_str();
+                image_tokens.insert(token.to_string());
             }
         }
     }
-
+    
     Ok(image_tokens.into_iter().collect())
 }
 
